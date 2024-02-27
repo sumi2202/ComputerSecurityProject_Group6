d4326f04914abbba4df3c2836c34757105f1514d
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2024-02-27 17:38:51 -0500

    views and authentication

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,9 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'laksumi somaskanthamoorthy'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth/')
     return app
